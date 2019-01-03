5f19755f6537efc9771207232950d5d083a21828
Daniel Bucket <danieljbucket@gmail.com>
2019-01-02 22:53:42 -0700

    trying to figure out how to dynamically generate playlist container elements and insert the appropriate playlist item elements inside of that.

--- src/components/Speaker/index.js
+++ src/components/Speaker/index.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import { SpeakerStyle, speakerPostition } from './SpeakerStyle';
+// import { Knob } from '../Knob';
 
 const Speaker = props => {
 	const { mouseEvent } = props;
 	return (
 		<SpeakerStyle style={speakerPostition(props)} levels={props.levels}>
 			{ props.position }
-			<div 	className="bass-speaker"
-							onClick={(i) => mouseEvent(i)}
-							onDragOver={e => this.onDragOver(e)}
-							draggable
-			>drag from here
-			</div>
-			<div 	className="bass-speaker"
-							onDragOver={i => console.log(i)}
-							onDrop={e => this.onDrop(e)}
-			>drop here </div>
+
 		</SpeakerStyle>
 		);
 };
